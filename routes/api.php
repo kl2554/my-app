a3a90f66c19eceefeaaede2302518620c7b8e8ec
krishna <krishnathisari2020@gmail.com>
2025-06-13 13:28:53 +0530

    api get metthod

--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
-use App\Http\Controllers\API\StudentController;
+use App\Http\Controllers\StudentController;
 
 Route::get('student', [StudentController::class, 'index'])->name('student.index');
 Route::post('student', [StudentController::class, 'store'])->name('student.store');
 Route::put('student/{id}', [StudentController::class, 'update'])->name('student.update');
 Route::get('student/{student}', [StudentController::class, 'show'])->name('student.show');
 Route::delete('student/{student}', [StudentController::class, 'destroy'])->name('student.destroy');
+
 
