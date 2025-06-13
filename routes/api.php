35d25f53e82e96bc65d549bd8d13ecd09671aa6a
krishna <krishnathisari2020@gmail.com>
2025-06-13 13:17:05 +0530

    change api.php

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,12 @@
 <?php
 
-
-
-
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 use App\Http\Controllers\API\StudentController;
 
-Route::apiResource('students', StudentController::class);
-
+Route::get('student', [StudentController::class, 'index'])->name('student.index');
+Route::post('student', [StudentController::class, 'store'])->name('student.store');
+Route::put('student/{id}', [StudentController::class, 'update'])->name('student.update');
+Route::get('student/{student}', [StudentController::class, 'show'])->name('student.show');
+Route::delete('student/{student}', [StudentController::class, 'destroy'])->name('student.destroy');
 
